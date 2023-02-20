bf08067449d90c06452a5fb038283587f4c0bcba
Zhuuus <zsmambayev@mail.ru>
2023-02-20 19:37:02 +0600

    init: initialize project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App =() =>{
+  return (
+    <div className="App">
+      <u1>
+       <p12>Hello world!</p12>
+       </u1>
+    </div>
+  );
+}
+
+export default App;
